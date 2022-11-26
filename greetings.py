0b1bf09310554bb9dc68e54d9fa2d1856a16a536
Kadiri2 <kadiridaudalinkedin@gmail.com>
2022-11-26 12:21:22 -0800

    made changes to line 4

--- greetings.py
+++ greetings.py
@@ -1,6 +1,6 @@
 name = input("what is your name: ")
 age = int(input(" What is your date of birth:  ")
-place = input("where are you from:" )
+place = input("that are you from:" )
 
 print(f"Hi {name}, you age is {age}, you are welcome")
 
